e1cf614e8f641e73988cfc26578064be46f22836
Esterling Accime <esterling.accime@coxautoinc.com>
2019-02-20 22:00:07 -0500

    Adding message

--- connection.js
+++ connection.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'netflix'
-});
+let connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'netflix'
+    });
+}
 connection.connect();
 
 
